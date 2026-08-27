94b69be2018cafc81b96dd2323215d99e908fa77
Jeff Bödtker <jeffbodtker@gmail.com>
2026-08-27 22:30:41 +0200

    Byt till nytt GA4-konto (G-DBCJ0J02D7), ta bort det gamla

--- js/cookie-consent.js
+++ js/cookie-consent.js
@@ -33,14 +33,14 @@ function applyConsent(consent) {
 
 function loadAnalytics() {
   const s = document.createElement('script');
-  s.src = 'https://www.googletagmanager.com/gtag/js?id=G-H8LWHGBDD5';
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=G-DBCJ0J02D7';
   s.async = true;
   document.head.appendChild(s);
   window.dataLayer = window.dataLayer || [];
   function gtag(){ dataLayer.push(arguments); }
   window.gtag = gtag;
   gtag('js', new Date());
-  gtag('config', 'G-H8LWHGBDD5');
+  gtag('config', 'G-DBCJ0J02D7');
 }
 
 function loadMarketing() {
