e653959281d12d6e5ad2ca79df03f22c048a716d
Jeff Bödtker <jeffbodtker@gmail.com>
2026-08-27 22:23:33 +0200

    Koppla in Google Analytics (GA4) via cookie-samtycket
    Mät-ID G-H8LWHGBDD5. Laddas endast efter att besökaren godkänt
    statistik-cookies i bannern, aldrig innan — i linje med integritets-
    policyn.

--- js/cookie-consent.js
+++ js/cookie-consent.js
@@ -32,15 +32,15 @@ function applyConsent(consent) {
 }
 
 function loadAnalytics() {
-  // TODO: Klistra in Google Analytics (GA4) här när mät-ID finns, t.ex.:
-  // const s = document.createElement('script');
-  // s.src = 'https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX';
-  // s.async = true;
-  // document.head.appendChild(s);
-  // window.dataLayer = window.dataLayer || [];
-  // function gtag(){ dataLayer.push(arguments); }
-  // gtag('js', new Date());
-  // gtag('config', 'G-XXXXXXX');
+  const s = document.createElement('script');
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=G-H8LWHGBDD5';
+  s.async = true;
+  document.head.appendChild(s);
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', 'G-H8LWHGBDD5');
 }
 
 function loadMarketing() {
